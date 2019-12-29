68800d63a09632d7c7d1d9ae53d580973e3c8884
Syed Samiul Islam (Monim) <ssi.monim@gmail.com>
2019-12-30 01:31:34 +0600

    code improvement

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
   <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Account Settings"><i class="fa fa-user"></i></a>
   <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Messages"><i class="fa fa-envelope"></i></a>
   <div class="w3-dropdown-hover w3-hide-small">
-    <button class="w3-button w3-padding-large" title="Notifications"><i class="fa fa-bell"></i><span id="co" class="w3-badge w3-right w3-small w3-green count_new_notification"></span></button>
+    <button class="w3-button w3-padding-large" title="Notifications"><i class="fa fa-bell"></i><span id="count_new_notification" class="w3-badge w3-right w3-small w3-green"></span></button>
     <div id="notifications" class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
       <!-- <a href="#" class="w3-bar-item w3-button">One new friend request</a>
       <a href="#" class="w3-bar-item w3-button">John Doe posted on your wall</a>
@@ -406,7 +406,7 @@ $(document).ready(function(){
       data:{request:req},
       success:function(data)
       {
-        $("#co").text(data);
+        $("#count_new_notification").text(data);
       }
      });
   }
